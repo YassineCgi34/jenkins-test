6021b7cec883ce67e7a1c256ea22738c65331c2c
YassineCgi34 <144351325+YassineCgi34@users.noreply.github.com>
2024-08-30 11:05:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, private  !");
+    // System.out.println("Hello, private  !");
+    System.out.println("Hello, yassine  !");
   }
 }
